c90901aec8b2f72175c8f043c1857960f09ea91f
Juan Camilo <juanccamilo74@gmail.com>
2026-02-26 13:26:47 -0500

    Segunda actividad terminada

--- app/src/main/java/com/example/maquetacionbasica/MainActivity.kt
+++ app/src/main/java/com/example/maquetacionbasica/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.maquetacionbasica.ui.theme.MaquetacionBasicaTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,7 +40,34 @@ fun PrimeraActividad (){
 @Preview(showSystemUi = true)
 @Composable
 fun SegundaActividad (){
-    Text("Hola mundo")
+    Column() {
+        Image(
+            painter = painterResource(id= R.drawable.Background2),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(top = 30.dp)
+        )
+        Text(
+            text="Jetpack compose tutorial",
+            fontWeight = FontWeight.Bold,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(16.dp)
+        )
+        Text(
+            text="Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, nd intuitive Kotlin APIS.",
+            modifier = Modifier
+                .padding(16.dp),
+            textAlign = TextAlign.Justify
+
+        )
+        Text(
+            text="In this tutorial, you build a simple Ul component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest Compose is built around Composable functions. These functions let you define your app's UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the Ul's construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.",
+            modifier = Modifier
+                .padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
 }
 
 // Tercera actividad
