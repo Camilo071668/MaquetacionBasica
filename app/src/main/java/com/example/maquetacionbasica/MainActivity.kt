2432e8d5e88c2fcfe96b2f298dd0bf97d9f72ddc
Juan Camilo <juanccamilo74@gmail.com>
2026-02-26 12:51:53 -0500

    My first commit

---
+++ app/src/main/java/com/example/maquetacionbasica/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.maquetacionbasica
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.maquetacionbasica.ui.theme.MaquetacionBasicaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Si()
+
+        }
+    }
+}
+@Preview(showSystemUi = true)
+@Composable
+fun Si(){
+    Text("Hola mundo")
+}
